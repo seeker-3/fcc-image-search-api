15ba48d8305962bfaa788ec260c19030ed60b0b7
Glitch (fcc-image-search-api-ab) <none>
2018-04-22 16:59:20 +0000

    🎁🌊 Checkpoint ./package.json:855406/51 ./app.js:855406/357

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const log = console.log;
 const url = process.env.MONGO_URI;
 const col = process.env.COLLECTION;
 
+import GoogleImageSearch from 'free-google-image-search'
 const {MongoClient} = require('mongodb');
 const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
 app.listen(process.env.PORT || 3000);
+
+// GoogleImageSearch.searchImage("cats").then((res) => {
+//     console.log(res);
+// });
 
 // MongoClient.connect(url, (err, client) => {
 //   if (err) throw err;
