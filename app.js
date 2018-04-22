d10ff6577865c282ed5c6956b5f40c38ed4a8332
Glitch (fcc-image-search-api-ab) <none>
2018-04-22 18:14:22 +0000

    🐉🎪 Checkpoint ./app.js:855406/399 ./package.json:855406/41

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const log = console.log;
 const url = process.env.MONGO_URI;
 const col = process.env.COLLECTION;
 const ID = process.env.CSE_ID;
-const key = process.env.API_KEY;
+const key = process.env.CSE_API_KEY;
 
 const {MongoClient} = require('mongodb');
 const GoogleImages = require('google-images');
-const client = new GoogleImages(ID, key);log(client);
+const imageSearch = require('node-google-image-search');
+const client = new GoogleImages(ID, key);
 const express = require('express')
 const app = express()
 
