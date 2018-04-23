afc5c328bf1a131dc7df3764ed6588ed1bdf7b79
Glitch (fcc-image-search-api-ab) <none>
2018-04-23 20:02:45 +0000

    💅🍝 Checkpoint ./package.json:855406/12 ./app.js:855406/717

--- app.js
+++ app.js
@@ -1,37 +1,23 @@
 const log = console.log;
 const url = process.env.MONGO_URI;
 const col = process.env.COLLECTION;
+const ID = process.env.ID;
+const KEY = process.env.KEY;
 
 
-const GoogleSearch = require('google-search');
-const client = new GoogleSearch({
-  key: process.env.CSE_API_KEY,
-  cx: process.env.CSE_ID,
-});
+const GoogleImages = require('google-images');
+const client = new GoogleImages(ID, KEY);
 const {MongoClient} = require('mongodb');
 const express = require('express')
 const app = express()
 
+//client.search('pizza').then(images => log(images));
 
 app.listen(process.env.PORT || 3000);
 //app.use(express.static('public'))
-
-// client.build({
-//   q: "cats",
-//   start: 5,
-//   fileType: "pdf",
-//   gl: "tr", //geolocation, 
-//   lr: "lang_tr",
-//   num: 10, // Number of search results to return between 1 and 10, inclusive 
-//   siteSearch: "http://kitaplar.ankara.edu.tr/" // Restricts results to URLs from a specified site 
-// }, function(error, response) {
-//   console.log(response);
-// });
-
-
-
-
 // MongoClient.connect(url, (err, client) => {
 //   if (err) throw err;
 //   client.close();
 // });
+
+
