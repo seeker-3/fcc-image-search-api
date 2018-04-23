4b200422caba59176df29bc21a8b81a3908500c6
Glitch (fcc-image-search-api-ab) <none>
2018-04-23 19:52:41 +0000

    🎟👩 Checkpoint ./package.json:855406/51 ./app.js:855406/976

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const log = console.log;
 const url = process.env.MONGO_URI;
 const col = process.env.COLLECTION;
-const ID = process.env.CSE_ID;
-const key = process.env.CSE_API_KEY;
 
+
+const GoogleSearch = require('google-search');
+const client = new GoogleSearch({
+  key: process.env.CSE_API_KEY,
+  cx: process.env.CSE_ID,
+});
 const {MongoClient} = require('mongodb');
-const GoogleImages = require('google-images');
-const imageSearch = require('node-google-image-search');
-const client = new GoogleImages(ID, key);
 const express = require('express')
 const app = express()
 
-//app.use(express.static('public'))
-app.listen(process.env.PORT || 3000);
 
-// client.search('Steve Angello').then(images => {
-//   log(images)
+app.listen(process.env.PORT || 3000);
+//app.use(express.static('public'))
+
+// client.build({
+//   q: "cats",
+//   start: 5,
+//   fileType: "pdf",
+//   gl: "tr", //geolocation, 
+//   lr: "lang_tr",
+//   num: 10, // Number of search results to return between 1 and 10, inclusive 
+//   siteSearch: "http://kitaplar.ankara.edu.tr/" // Restricts results to URLs from a specified site 
+// }, function(error, response) {
+//   console.log(response);
 // });
+
+
 
 
 // MongoClient.connect(url, (err, client) => {
