574b052ca0c0f5f79c25cb95307ce50d04a2ff4c
Glitch (fcc-image-search-api-ab) <none>
2018-04-24 04:10:51 +0000

    😥🎿 Checkpoint ./app.js:855406/308

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const KEY = process.env.KEY;
 
 const url = require('url');
 
-const client = new (require('google-images'))(ID, KEY);
+const searchClient = new (require('google-images'))(ID, KEY);
 const {MongoClient} = require('mongodb');
 const app = require('express')();
 
 function getClient(callback, next) {
   MongoClient.connect(db_url, (err, client) => {
     if (err) throw err;
-    callback(client);
+    callback(client);log(next)
     next();
   });
 }
@@ -22,34 +22,37 @@ function getClient(callback, next) {
 app.get('/', (req, res) => res.end());
 
 app.get('/history', (req, res) => {
-  var cleint;
-  getClient(c => client = c, find);
-  const find = () =>
-  client.db().collection(col).find({}).toArray((err, docs) => {
-    res.json(docs.sort((a, b) => new Date(b.date) - new Date(a.date)).map(x => ({
-      query: x.query,
-      when: x.date,
-    })).slice(0,30));
-    client.close();
-  });
+  var client; getClient(c => client = c, find);
+  
+  function find() {
+    client.db().collection(col).find({}).toArray((err, docs) => {
+      res.json(docs.sort((a, b) => new Date(b.date) - new Date(a.date)).map(x => ({
+        query: x.query,
+        when: x.date,
+      })).slice(0,30));
+      client.close();
+    });
+  }
 });
 
 app.get('/search/*', (req, res) => {
   const query = url.parse(req.url).pathname.slice(8);
   
-  client.search(query, {page: req.query.offset || 1}).then(images => {
+  var client;
+  searchClient.search(query, {page: req.query.offset || 1}).then(images => {
     res.json(images);
-    var client;
     getClient(c => client = c, insert);
   });
   
-  const insert = () => client.db().collection(col).insert({
-    query: query,
-    date: new Date,
-  }, err => {
-    if (err) throw err;
-    client.close();
-  });
+  function insert() {
+    client.db().collection(col).insert({
+      query: query,
+      date: new Date,
+    }, err => {
+      if (err) throw err;
+      client.close();
+    });
+  }
 });
 
 app.listen(process.env.PORT || 3000);
